7fe99476c68b44fe31a15ca1c4b28d39a552f4c7
Leonid <leonidb93@gmail.com>
2021-01-16 14:32:27 +0100

    start implement ui

---
+++ RealtimeInteractiveCrawler/Game/UI/UIBase.cs
@@ -0,0 +1,26 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+
+namespace RealtimeInteractiveCrawler
+{
+    abstract class UIBase : Transformable, Drawable
+    {
+        public new Vector2i Position
+        {
+            get { return (Vector2i)base.Position; }
+            set { base.Position = (Vector2f)value; }
+        }
+
+        public new Vector2i Origin
+        {
+            get { return (Vector2i)base.Origin; }
+            set { base.Origin = (Vector2f)value; }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+
+        }
+    }
+}
